28fd628c827a0cd29301e4ec3fb5b7172725d5a8
Felipe Britto <105891789+Felipe-M-D-B@users.noreply.github.com>
2026-09-23 15:40:58 -0300

    Configuração inicial do projeto OmniLeito

---
+++ src/test/java/unesp/com/OmniLeito/OmniLeitoApplicationTests.java
@@ -0,0 +1,13 @@
+package unesp.com.OmniLeito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniLeitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
